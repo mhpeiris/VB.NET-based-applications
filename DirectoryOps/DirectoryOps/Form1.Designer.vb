4cd15fb0d4f9d591263462ca12682837cae6586f
Hareen Peiris <hareenmpeiris@gmail.com>
2017-03-30 23:40:25 -0400

    Add files via upload

---
+++ DirectoryOps/DirectoryOps/Form1.Designer.vb
@@ -0,0 +1,309 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.OperationToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NewDirectoryToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CopyDirectoryToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CopyFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripButton3 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripButton4 = New System.Windows.Forms.ToolStripButton()
+        Me.OkButton = New System.Windows.Forms.Button()
+        Me.CancelButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.BrowserButton = New System.Windows.Forms.Button()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.BrowserLabel = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.FileTextBox = New System.Windows.Forms.TextBox()
+        Me.newPathLabel = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1.SuspendLayout()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OperationToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(596, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'OperationToolStripMenuItem
+        '
+        Me.OperationToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewDirectoryToolStripMenuItem, Me.CopyDirectoryToolStripMenuItem, Me.CopyFileToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.OperationToolStripMenuItem.Name = "OperationToolStripMenuItem"
+        Me.OperationToolStripMenuItem.Size = New System.Drawing.Size(72, 20)
+        Me.OperationToolStripMenuItem.Text = "Operation"
+        '
+        'NewDirectoryToolStripMenuItem
+        '
+        Me.NewDirectoryToolStripMenuItem.Name = "NewDirectoryToolStripMenuItem"
+        Me.NewDirectoryToolStripMenuItem.Size = New System.Drawing.Size(153, 22)
+        Me.NewDirectoryToolStripMenuItem.Text = "New Directory"
+        '
+        'CopyDirectoryToolStripMenuItem
+        '
+        Me.CopyDirectoryToolStripMenuItem.Name = "CopyDirectoryToolStripMenuItem"
+        Me.CopyDirectoryToolStripMenuItem.Size = New System.Drawing.Size(153, 22)
+        Me.CopyDirectoryToolStripMenuItem.Text = "Copy Directory"
+        '
+        'CopyFileToolStripMenuItem
+        '
+        Me.CopyFileToolStripMenuItem.Name = "CopyFileToolStripMenuItem"
+        Me.CopyFileToolStripMenuItem.Size = New System.Drawing.Size(153, 22)
+        Me.CopyFileToolStripMenuItem.Text = "Copy File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(153, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton1, Me.ToolStripButton2, Me.ToolStripSeparator1, Me.ToolStripButton3, Me.ToolStripSeparator2, Me.ToolStripButton4})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 24)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(596, 25)
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton1.Image = Global.DirectoryOps.My.Resources.Resources.new_dir
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButton1.Text = "ToolStripButton1"
+        '
+        'ToolStripButton2
+        '
+        Me.ToolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton2.Image = Global.DirectoryOps.My.Resources.Resources.copy_dir
+        Me.ToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton2.Name = "ToolStripButton2"
+        Me.ToolStripButton2.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButton2.Text = "ToolStripButton2"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripButton3
+        '
+        Me.ToolStripButton3.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton3.Image = Global.DirectoryOps.My.Resources.Resources.copy_file
+        Me.ToolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton3.Name = "ToolStripButton3"
+        Me.ToolStripButton3.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButton3.Text = "ToolStripButton3"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripButton4
+        '
+        Me.ToolStripButton4.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton4.Image = Global.DirectoryOps.My.Resources.Resources.About_icon
+        Me.ToolStripButton4.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton4.Name = "ToolStripButton4"
+        Me.ToolStripButton4.Size = New System.Drawing.Size(23, 22)
+        Me.ToolStripButton4.Text = "ToolStripButton4"
+        '
+        'OkButton
+        '
+        Me.OkButton.Enabled = False
+        Me.OkButton.Location = New System.Drawing.Point(159, 275)
+        Me.OkButton.Name = "OkButton"
+        Me.OkButton.Size = New System.Drawing.Size(75, 23)
+        Me.OkButton.TabIndex = 2
+        Me.OkButton.Text = "OK"
+        Me.OkButton.UseVisualStyleBackColor = True
+        '
+        'CancelButton
+        '
+        Me.CancelButton.Enabled = False
+        Me.CancelButton.Location = New System.Drawing.Point(270, 275)
+        Me.CancelButton.Name = "CancelButton"
+        Me.CancelButton.Size = New System.Drawing.Size(75, 23)
+        Me.CancelButton.TabIndex = 3
+        Me.CancelButton.Text = "Cancel"
+        Me.CancelButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(112, 65)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 20)
+        Me.Label1.TabIndex = 4
+        '
+        'BrowserButton
+        '
+        Me.BrowserButton.Enabled = False
+        Me.BrowserButton.Location = New System.Drawing.Point(33, 115)
+        Me.BrowserButton.Name = "BrowserButton"
+        Me.BrowserButton.Size = New System.Drawing.Size(75, 23)
+        Me.BrowserButton.TabIndex = 5
+        Me.BrowserButton.Text = "Browser"
+        Me.BrowserButton.UseVisualStyleBackColor = True
+        Me.BrowserButton.Visible = False
+        '
+        'BrowserLabel
+        '
+        Me.BrowserLabel.AutoSize = True
+        Me.BrowserLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BrowserLabel.Location = New System.Drawing.Point(172, 124)
+        Me.BrowserLabel.Name = "BrowserLabel"
+        Me.BrowserLabel.Size = New System.Drawing.Size(0, 16)
+        Me.BrowserLabel.TabIndex = 6
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(172, 171)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 15)
+        Me.Label2.TabIndex = 7
+        '
+        'FileTextBox
+        '
+        Me.FileTextBox.Enabled = False
+        Me.FileTextBox.Location = New System.Drawing.Point(175, 225)
+        Me.FileTextBox.Name = "FileTextBox"
+        Me.FileTextBox.Size = New System.Drawing.Size(151, 20)
+        Me.FileTextBox.TabIndex = 8
+        Me.FileTextBox.Visible = False
+        '
+        'newPathLabel
+        '
+        Me.newPathLabel.AutoSize = True
+        Me.newPathLabel.Location = New System.Drawing.Point(12, 232)
+        Me.newPathLabel.Name = "newPathLabel"
+        Me.newPathLabel.Size = New System.Drawing.Size(0, 13)
+        Me.newPathLabel.TabIndex = 9
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(0, 365)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(596, 23)
+        Me.ProgressBar1.TabIndex = 10
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(596, 389)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.newPathLabel)
+        Me.Controls.Add(Me.FileTextBox)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.BrowserLabel)
+        Me.Controls.Add(Me.BrowserButton)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.CancelButton)
+        Me.Controls.Add(Me.OkButton)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "DirectoryOps (Mahathelge Hareen Peiris)"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents OperationToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NewDirectoryToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopyDirectoryToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopyFileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripButton1 As ToolStripButton
+    Friend WithEvents ToolStripButton2 As ToolStripButton
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ToolStripButton3 As ToolStripButton
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents ToolStripButton4 As ToolStripButton
+    Friend WithEvents OkButton As Button
+    Friend WithEvents CancelButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents BrowserButton As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents BrowserLabel As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents FileTextBox As TextBox
+    Friend WithEvents newPathLabel As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ DirectoryOps/DirectoryOps/Form1.vb
@@ -0,0 +1,154 @@
+﻿Public Class Form1
+    Public path As String
+    Public newDirectory As Boolean = False
+    Public copyDirectory As Boolean = False
+    Public copyFile As Boolean = False
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        AboutBox1.ShowDialog()
+    End Sub
+
+    Private Sub ToolStripButton4_Click(sender As Object, e As EventArgs) Handles ToolStripButton4.Click
+        AboutBox1.ShowDialog()
+    End Sub
+
+    Private Sub NewDirectoryToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NewDirectoryToolStripMenuItem.Click, ToolStripButton1.Click
+        Label1.Text = "Create in destination directory"
+        newDirectory = True
+        copyDirectory = False
+        copyFile = False
+        CancelButton.Enabled = True
+        BrowserButton.Enabled = True
+        BrowserButton.Visible = True
+        BrowserLabel.Text = ""
+        Label2.Text = ""
+        FileTextBox.Enabled = False
+        FileTextBox.Visible = False
+        FileTextBox.Text = ""
+        newPathLabel.Text = ""
+        ProgressBar1.Value = 0
+    End Sub
+
+    Private Sub CopyDirectoryToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyDirectoryToolStripMenuItem.Click, ToolStripButton2.Click
+        Label1.Text = "Copy this directory"
+        copyDirectory = True
+        newDirectory = False
+        copyFile = False
+        CancelButton.Enabled = True
+        BrowserButton.Enabled = True
+        BrowserButton.Visible = True
+        BrowserLabel.Text = ""
+        Label2.Text = ""
+        FileTextBox.Enabled = False
+        FileTextBox.Visible = False
+        FileTextBox.Text = ""
+        newPathLabel.Text = ""
+        ProgressBar1.Value = 0
+    End Sub
+
+    Private Sub CopyFileToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CopyFileToolStripMenuItem.Click, ToolStripButton3.Click
+        Label1.Text = "Copy this file"
+        copyFile = True
+        newDirectory = False
+        copyDirectory = False
+        CancelButton.Enabled = True
+        BrowserButton.Enabled = True
+        BrowserButton.Visible = True
+        BrowserLabel.Text = ""
+        Label2.Text = ""
+        FileTextBox.Enabled = False
+        FileTextBox.Visible = False
+        FileTextBox.Text = ""
+        newPathLabel.Text = ""
+        ProgressBar1.Value = 0
+    End Sub
+
+    Private Sub BrowserButton_Click(sender As Object, e As EventArgs) Handles BrowserButton.Click
+        With FolderBrowserDialog1
+            .SelectedPath = ""
+            If newDirectory = True Then
+                If .ShowDialog = System.Windows.Forms.DialogResult.OK Then
+                    BrowserLabel.Text = .SelectedPath + "\"
+                    Label2.Text = "this new subdirectory:"
+                    FileTextBox.Enabled = True
+                    FileTextBox.Visible = True
+                    path = .SelectedPath + "\"
+                End If
+            End If
+        End With
+        With FolderBrowserDialog1
+            If copyDirectory = True Then
+                If .ShowDialog = System.Windows.Forms.DialogResult.OK Then
+                    BrowserLabel.Text = .SelectedPath + "\"
+                    Label2.Text = "to:"
+                    FileTextBox.Enabled = True
+                    FileTextBox.Visible = True
+                    path = .SelectedPath + "\"
+                    newPathLabel.Text = System.IO.Path.GetDirectoryName(.SelectedPath) + "\"
+                End If
+            End If
+        End With
+
+        If copyFile = True Then
+            With OpenFileDialog1
+                .InitialDirectory = ""
+                If .ShowDialog = System.Windows.Forms.DialogResult.OK Then
+                    BrowserLabel.Text = .FileName
+                    Label2.Text = "to:"
+                    FileTextBox.Enabled = True
+                    FileTextBox.Visible = True
+                    path = .FileName
+                    newPathLabel.Text = System.IO.Path.GetDirectoryName(.FileName) + "\"
+                End If
+            End With
+        End If
+    End Sub
+
+    Private Sub OkButton_Click(sender As Object, e As EventArgs) Handles OkButton.Click
+        Timer1.Start()
+        If newDirectory = True Then
+            My.Computer.FileSystem.CreateDirectory(path + FileTextBox.Text)
+            newDirectory = False
+        ElseIf copyDirectory = True Then
+            My.Computer.FileSystem.CopyDirectory(path, newPathLabel.Text + FileTextBox.Text)
+            copyDirectory = False
+        ElseIf copyFile = True Then
+            My.Computer.FileSystem.CopyFile(path, newPathLabel.Text + FileTextBox.Text)
+            copyFile = False
+        End If
+    End Sub
+
+    Private Sub FileTextBox_TextChanged(sender As Object, e As EventArgs) Handles FileTextBox.TextChanged
+        If FileTextBox.Text = "" Then
+            OkButton.Enabled = False
+        Else
+            OkButton.Enabled = True
+        End If
+    End Sub
+
+    Private Sub CancelButton_Click(sender As Object, e As EventArgs) Handles CancelButton.Click
+        Label1.Text = ""
+        CancelButton.Enabled = False
+        OkButton.Enabled = False
+        BrowserButton.Enabled = False
+        BrowserButton.Visible = False
+        BrowserLabel.Text = ""
+        newPathLabel.Text = ""
+        Label2.Text = ""
+        FileTextBox.Enabled = False
+        FileTextBox.Visible = False
+        ProgressBar1.Value = 0
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ProgressBar1.Increment(10)
+        If ProgressBar1.Value = 100 Then
+            Timer1.Stop()
+        End If
+    End Sub
+
+End Class
