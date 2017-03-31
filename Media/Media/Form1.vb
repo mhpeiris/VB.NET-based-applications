6111821c44e4c82564f53fc532f060ed49b646a4
Hareen Peiris <hareenmpeiris@gmail.com>
2017-03-30 23:44:39 -0400

    Add files via upload

---
+++ Media/Media/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Button1.Enabled = True
+        Button2.Enabled = False
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        PictureBox1.Image = My.Resources.canada_medium_animated
+        My.Computer.Audio.Play(My.Resources.canada, AudioPlayMode.Background)
+        Button1.Enabled = False
+        Button2.Enabled = True
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        PictureBox1.Image = My.Resources.canada_still
+        My.Computer.Audio.Stop()
+        Button1.Enabled = True
+        Button2.Enabled = False
+    End Sub
+End Class
