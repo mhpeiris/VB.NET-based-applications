0aa82462902ef472e418718f6d96895981126698
Hareen Peiris <hareenmpeiris@gmail.com>
2017-03-30 23:42:32 -0400

    Drag and Drop

---
+++ DragAndDrop/DragAndDrop/DragAndDrop.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class DragAndDrop
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.StatusBar1 = New System.Windows.Forms.StatusBar()
+        Me.StatusBarPanel1 = New System.Windows.Forms.StatusBarPanel()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.StatusBarPanel1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'StatusBar1
+        '
+        Me.StatusBar1.Location = New System.Drawing.Point(0, 316)
+        Me.StatusBar1.Name = "StatusBar1"
+        Me.StatusBar1.Panels.AddRange(New System.Windows.Forms.StatusBarPanel() {Me.StatusBarPanel1})
+        Me.StatusBar1.Size = New System.Drawing.Size(452, 22)
+        Me.StatusBar1.TabIndex = 2
+        Me.StatusBar1.Text = "StatusBar"
+        '
+        'StatusBarPanel1
+        '
+        Me.StatusBarPanel1.Name = "StatusBarPanel1"
+        Me.StatusBarPanel1.Text = "Point"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Cursor = System.Windows.Forms.Cursors.Default
+        Me.PictureBox2.Image = Global.DragAndDrop.My.Resources.Resources.smiley
+        Me.PictureBox2.Location = New System.Drawing.Point(22, 43)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(164, 161)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.PictureBox1.Image = Global.DragAndDrop.My.Resources.Resources.dab2
+        Me.PictureBox1.Location = New System.Drawing.Point(206, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(154, 160)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'DragAndDrop
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(452, 338)
+        Me.Controls.Add(Me.StatusBar1)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "DragAndDrop"
+        Me.Text = "Drag And Drop (Mahathelge Hareen Peiris)"
+        CType(Me.StatusBarPanel1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents StatusBar1 As StatusBar
+    Friend WithEvents StatusBarPanel1 As StatusBarPanel
+End Class
---
+++ DragAndDrop/DragAndDrop/DragAndDrop.vb
@@ -0,0 +1,100 @@
+﻿Public Class DragAndDrop
+    Private isDragging1 As Boolean = False, isDragging2 As Boolean = False
+    Private startPoint1 As Point, startPoint2 As Point
+
+    Private Sub DragAndDrop_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        initialize()
+    End Sub
+
+    Public Sub initialize()
+        If isDragging1 Then
+            isDragging2 = False
+        End If
+
+        If isDragging2 Then
+            isDragging1 = False
+        End If
+    End Sub
+
+    'PictureBox 1 starts
+    Private Sub PictureBox1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseDown, PictureBox2.MouseDown
+        startPoint1 = PictureBox1.PointToScreen(New Point(e.X, e.Y))
+        isDragging1 = True
+        isDragging2 = False
+        PictureBox1.BringToFront()
+        Me.Cursor = New Cursor(New System.IO.MemoryStream(My.Resources.H_MOVE))
+        StatusBar1.Text = "PictureBox1: " + startPoint1.X.ToString + "," + startPoint1.Y.ToString
+        If PictureBox1.Bounds.IntersectsWith(PictureBox2.Bounds) Then
+            StatusBar1.Text += " || " + "PictureBox1 currenctly over PictureBox2"
+        End If
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseMove
+        If isDragging1 Then
+            Dim endPoint1 As Point = PictureBox1.PointToScreen(New Point(e.X, e.Y))
+            PictureBox1.Left += (endPoint1.X - startPoint1.X)
+            PictureBox1.Top += (endPoint1.Y - startPoint1.Y)
+            startPoint1 = endPoint1
+            isDragging2 = False
+            PictureBox1.BringToFront()
+            Me.Cursor = New Cursor(New System.IO.MemoryStream(My.Resources.H_MOVE))
+            StatusBar1.Text = "PictureBox1: " + startPoint1.X.ToString + "," + startPoint1.Y.ToString
+            If PictureBox1.Bounds.IntersectsWith(PictureBox2.Bounds) Then
+                StatusBar1.Text += " || " + "PictureBox1 currenctly over PictureBox2"
+            End If
+        End If
+    End Sub
+
+    Private Sub PictureBox1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseUp
+        isDragging1 = False
+        isDragging2 = False
+        PictureBox1.BringToFront()
+        StatusBar1.Text = "PictureBox1: " + startPoint1.X.ToString + "," + startPoint1.Y.ToString
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        If PictureBox1.Bounds.IntersectsWith(PictureBox2.Bounds) Then
+            StatusBar1.Text += " || " + "PictureBox1 currenctly over PictureBox2"
+        End If
+    End Sub
+    'PictureBox 1 ends
+
+    'PictureBox 2 starts
+    Private Sub PictureBox2_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox2.MouseDown
+        startPoint2 = PictureBox2.PointToScreen(New Point(e.X, e.Y))
+        isDragging2 = True
+        isDragging1 = False
+        PictureBox2.BringToFront()
+        Me.Cursor = New Cursor(New System.IO.MemoryStream(My.Resources.H_MOVE))
+        StatusBar1.Text = "PictureBox2: " + startPoint2.X.ToString + "," + startPoint2.Y.ToString
+        If PictureBox2.Bounds.IntersectsWith(PictureBox1.Bounds) Then
+            StatusBar1.Text += " || " + "PictureBox2 currenctly over PictureBox1"
+        End If
+    End Sub
+
+    Private Sub PictureBox2_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox2.MouseMove
+        If isDragging2 Then
+            Dim endPoint2 As Point = PictureBox2.PointToScreen(New Point(e.X, e.Y))
+            PictureBox2.Left += (endPoint2.X - startPoint2.X)
+            PictureBox2.Top += (endPoint2.Y - startPoint2.Y)
+            startPoint2 = endPoint2
+            isDragging1 = False
+            PictureBox2.BringToFront()
+            Me.Cursor = New Cursor(New System.IO.MemoryStream(My.Resources.H_MOVE))
+            StatusBar1.Text = "PictureBox2: " + startPoint2.X.ToString + "," + startPoint2.Y.ToString
+            If PictureBox2.Bounds.IntersectsWith(PictureBox1.Bounds) Then
+                StatusBar1.Text += " || " + "PictureBox2 currenctly over PictureBox1"
+            End If
+        End If
+    End Sub
+
+    Private Sub PictureBox2_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox2.MouseUp
+        isDragging2 = False
+        isDragging1 = False
+        PictureBox2.BringToFront()
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        StatusBar1.Text = "PictureBox2: " + startPoint2.X.ToString + "," + startPoint2.Y.ToString
+        If PictureBox2.Bounds.IntersectsWith(PictureBox1.Bounds) Then
+            StatusBar1.Text += " || " + "PictureBox2 currenctly over PictureBox1"
+        End If
+    End Sub
+    'PictureBox 2 ends
+End Class
